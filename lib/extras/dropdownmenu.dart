474fb9b4861764f2debce9447ae6661b21551170
chris-netizen <adedejisomoye1@gmail.com>
2023-01-03 22:48:33 +0000

    new_changes

---
+++ lib/extras/dropdownmenu.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:milsat_management_app/screens/Interns/interns.dart';
+
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+}
+
+class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.keyboard_arrow_down),
+      style: const TextStyle(
+        color: Color(0xFF5E545F),
+      ),
+      elevation: 16,
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
